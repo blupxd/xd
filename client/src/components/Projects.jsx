07937d456aca7d17b1c608e392481425c430df4b
blupxd <matija.stefanovic050@gmail.com>
2024-03-14 16:42:56 +0100

    xd

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useRef } from 'react';
 import { motion, useInView, useAnimation } from "framer-motion";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import aros from '../images/aros.png';
+import alppro from '../images/alppro.png'
 
 const Projects = () => {
   const ref = useRef(null);
@@ -19,6 +21,37 @@ const Projects = () => {
       mainControls2.start("visible");
     }
   }, [isInView, isInView2]);
+
+  const projectCard = (image, naziv, opis, link, index) => {
+    const isEven = index % 2 === 0;
+    const flexDirection = isEven ? 'md:flex-row' : 'md:flex-row-reverse';
+  
+    return (
+      <div className={`flex flex-col ${flexDirection} border-b-[2px] border-pink-700`}>
+        <div className='relative w-full md:w-1/2 h-96 overflow-hidden group'>
+          <img
+            src={image} 
+            alt={naziv}
+            className="w-full h-full object-cover group-hover:opacity-50 transition-opacity duration-300"
+          />
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <a href={link} className="border-pink-700 border-[2px] text-pink-700 font-bold hover:bg-pink-700 hover:text-white transition-all duration-300 py-2 px-4">
+              View website
+            </a>
+          </div>
+        </div>
+        <div className="p-12 h-96 md:w-1/2 ">
+          <h3 className="text-4xl text-yellow-300 font-semibold mb-2 flex items-center">
+            {naziv}
+          </h3>
+          <p className="text-gray-600">{opis}</p>
+          <a href={link} className="text-pink-700 mt-2 block hover:underline">
+            Visit website <FontAwesomeIcon className='text-sm' icon={faArrowUpRightFromSquare} />
+          </a>
+        </div>
+      </div>
+    );
+  };
 
   return (
     <div id="projects" className='flex flex-col'>
@@ -46,33 +79,26 @@ const Projects = () => {
           initial="hidden"
           animate={mainControls2}
           transition={{ duration: 0.5, delay: 0.25 }}
-          className={`flex flex-col sticky top-0 md:flex-row overflow-hidden relative z-10 ${isInView2 ? 'even:flex-row-reverse' : 'odd:flex-row'}`}
+          className={`flex flex-col sticky top-0 relative z-10 ${isInView2 ? 'even:flex-row-reverse' : 'odd:flex-row'}`}
         >
-          <div className="relative w-full md:w-1/2 h-96 overflow-hidden group">
-            <img
-              src="https://www.site-shot.com/cached_image/QK0jPNZBEe60YQJCrBEAAg" 
-              alt="Projekat 1"
-              className="w-full h-full object-cover group-hover:opacity-50 transition-opacity duration-300"
-            />
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-              <a href="#" className="border-pink-700 border-[2px] text-pink-700 font-bold hover:bg-pink-700 hover:text-white trainsition-all duration-300 py-2 px-4">
-                View website
-              </a>
-            </div>
-          </div>
-          <div className="p-12">
-            <h3 className="text-4xl text-yellow-300 font-semibold mb-2 flex items-center">
-              Projekat 1<FontAwesomeIcon className='ml-4 text-2xl' icon={faArrowUpRightFromSquare} />
-              </h3>
-            <p className="text-gray-600">Opis projekta 1...</p>
-            <a href="#" className="text-pink-700 mt-2 block hover:underline">
-              Visit website
-            </a>
-          </div>
+          {projectCard(
+            aros,
+            'AROS Dekanti',
+            'AROS Dekanti is a website that I have been working on using ReactJS and NodeJS backend to help the customer upload their perfumes with ease.',
+            'https://arosdekant.vercel.app/',
+            1
+          )}
+          {projectCard(
+            alppro,
+            'ALP-PRO Team',
+            'ALP-PRO Team is a firm that does alpinism work. On this website I have used ReactJS and also ExpressJS server that connects to MongoDB and Firebase for image storage and data storage for uploading images onto their website',
+            'https://alp-proteam.vercel.app/',
+            2
+          )}
         </motion.div>
       </div>
     </div>
   );
-}
+};
 
 export default Projects;
